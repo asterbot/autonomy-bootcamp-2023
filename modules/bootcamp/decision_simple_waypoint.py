434d95a811b30edcf8f8be9c1a4960133449c992
asterbot <arjunsodhi7@gmail.com>
2024-09-10 18:11:14 -0400

    minor fix

--- modules/bootcamp/decision_simple_waypoint.py
+++ modules/bootcamp/decision_simple_waypoint.py
@@ -69,13 +69,23 @@ class DecisionSimpleWaypoint(base_decision.BaseDecision):
         # ============
 
         # Do something based on the report and the state of this class...
+        def in_range(report: drone_report.DroneReport) -> bool:
+            """
+            Returns bool which denotes whether the waypoint location is in range (by self.acceptance radius)
+            of the drones current location
+            """
+            rel_x = self.waypoint.location_x - report.position.location_x
+            rel_y = self.waypoint.location_y - report.position.location_y
+            distance_squared = (rel_x * rel_x) + (rel_y * rel_y)
+            return distance_squared <= (self.acceptance_radius) * (self.acceptance_radius)
+
 
         status = report.status
         rel_x = self.waypoint.location_x - report.position.location_x
         rel_y = self.waypoint.location_y - report.position.location_y
 
         # Logic: If drone is not in range, keep moving towards the waypoint
-        if self.in_range(report):
+        if in_range(report):
             if status == drone_status.DroneStatus.HALTED:
                 command = commands.Command.create_land_command()
             else:
@@ -88,13 +98,3 @@ class DecisionSimpleWaypoint(base_decision.BaseDecision):
         # ============
 
         return command
-
-    def in_range(self, report: drone_report.DroneReport) -> bool:
-        """
-        Returns bool which denotes whether the waypoint location is in range (by self.acceptance radius)
-        of the drones current location
-        """
-        rel_x = self.waypoint.location_x - report.position.location_x
-        rel_y = self.waypoint.location_y - report.position.location_y
-        distance_squared = (rel_x * rel_x) + (rel_y * rel_y)
-        return distance_squared <= (self.acceptance_radius) * (self.acceptance_radius)
